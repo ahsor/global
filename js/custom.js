da07b45a125c50a9d73cb0c5ddf2468e1e5f8482
ahsor <jemicom@naver.com>
2022-10-27 13:31:48 +0900

    first commit

---
+++ js/custom.js
@@ -0,0 +1,28 @@
+﻿$(document).ready(function(){  	
+	$('.btnMenu').on('click', function(e){
+		e.preventDefault();
+		$('#gnbMo').fadeIn();
+	})
+	
+	$('#gnbMo').on('click', function(e){
+		e.preventDefault();
+		$(this).fadeOut();
+	})
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
